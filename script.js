3d972ce718d79c3e03118886dcbf925edb05602f
Ngô ĐứcTâm <133946215+Moobbot2@users.noreply.github.com>
2023-12-10 17:26:47 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function spinWheel() {
+    // Xử lý quay vòng ở đây
+    // Lấy giá trị email từ input
+    var email = $('#email').val();
+
+    // Gửi email và nhận phần quà từ server (cần phải triển khai một server để xử lý)
+
+    // Thêm logic xử lý quay vòng và hiển thị kết quả
+    // ...
+
+    // Lưu kết quả vào file Excel (cần phải triển khai một phía server để thực hiện)
+    // ...
+
+    // Chạy nhạc nền
+    document.getElementById('backgroundMusic').play();
+}
